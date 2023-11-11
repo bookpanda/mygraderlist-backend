1c55a5d0c9f0a71ab01fef0151732844e14d26e5
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-12 01:12:19 +0700

    feat: seeder

---
+++ src/database/seeds/courses.seed.go
@@ -0,0 +1,23 @@
+package seed
+
+import (
+	"github.com/bookpanda/mygraderlist-backend/src/app/model/course"
+)
+
+var courses = []course.Course{
+	{
+		CourseCode: "liked",
+		Name:       "Liked Problems",
+		Color:      "#6114c7",
+	},
+	{
+		CourseCode: "2110211",
+		Name:       "Data Structure",
+		Color:      "#0285c7",
+	},
+	{
+		CourseCode: "2110263",
+		Name:       "Digital Logic Lab",
+		Color:      "#854d0e",
+	},
+}
