8ef7e63487b02c1e034268abf705c5f60cecbbd9
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-11 23:20:09 +0700

    feat: db connection

---
+++ src/database/mysql.connection.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+	"os/user"
+	"strconv"
+
+	"github.com/bookpanda/mygraderlist-backend/src/app/model/course"
+	"github.com/bookpanda/mygraderlist-backend/src/app/model/emoji"
+	"github.com/bookpanda/mygraderlist-backend/src/app/model/like"
+	"github.com/bookpanda/mygraderlist-backend/src/app/model/problem"
+	"github.com/bookpanda/mygraderlist-backend/src/app/model/rating"
+	"github.com/bookpanda/mygraderlist-backend/src/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDatabase(conf *config.Database) (db *gorm.DB, err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True", conf.User, conf.Password, conf.Host, strconv.Itoa(conf.Port), conf.Name)
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&user.User{}, &course.Course{}, &problem.Problem{}, &rating.Rating{}, &emoji.Emoji{}, &like.Like{})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
---
+++ src/database/redis.connection.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"github.com/bookpanda/mygraderlist-backend/src/config"
+	"github.com/go-redis/redis/v8"
+)
+
+func InitRedisConnect(conf *config.Redis) (cache *redis.Client, err error) {
+	cache = redis.NewClient(&redis.Options{
+		Addr: conf.Host,
+		DB:   3,
+	})
+
+	return
+}
