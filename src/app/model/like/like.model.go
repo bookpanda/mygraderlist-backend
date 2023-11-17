78052ea5ecb34eb195f2dbcd87adb97e9925bbec
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-17 15:54:28 +0700

    feat: unique ids

--- src/app/model/like/like.model.go
+++ src/app/model/like/like.model.go
@@ -9,8 +9,8 @@ import (
 
 type Like struct {
 	model.Base
-	ProblemID *uuid.UUID       `json:"problem_id"`
+	ProblemID *uuid.UUID       `json:"problem_id" gorm:"index:idx_name,unique"`
 	Problem   *problem.Problem `json:"problem" gorm:"foreignKey:ProblemID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
-	UserID    *uuid.UUID       `json:"user_id"`
+	UserID    *uuid.UUID       `json:"user_id" gorm:"index:idx_name,unique"`
 	User      *user.User       `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
 }
--- src/app/model/rating/rating.model.go
+++ src/app/model/rating/rating.model.go
@@ -11,8 +11,8 @@ type Rating struct {
 	model.Base
 	Score      int              `json:"score" gorm:"type:tinyint"`
 	Difficulty int              `json:"difficulty" gorm:"type:tinyint"`
-	ProblemID  *uuid.UUID       `json:"problem_id"`
+	ProblemID  *uuid.UUID       `json:"problem_id" gorm:"index:idx_name,unique"`
 	Problem    *problem.Problem `json:"problem" gorm:"foreignKey:ProblemID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
-	UserID     *uuid.UUID       `json:"user_id"`
+	UserID     *uuid.UUID       `json:"user_id" gorm:"index:idx_name,unique"`
 	User       *user.User       `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
 }
