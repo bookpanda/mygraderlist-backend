b2e9ff5d151abd36447a46e6acc306e47f58d97a
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-17 17:02:33 +0700

    feat: remove soft delete

--- src/app/model/common.model.go
+++ src/app/model/common.model.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Base struct {
-	ID        uuid.UUID      `json:"id" gorm:"primaryKey;size:191"`
-	CreatedAt time.Time      `json:"created_at" gorm:"type:datetime;autoCreateTime:nano"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"type:datetime;autoUpdateTime:nano"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:datetime"`
+	ID        uuid.UUID `json:"id" gorm:"primaryKey;size:191"`
+	CreatedAt time.Time `json:"created_at" gorm:"type:datetime;autoCreateTime:nano"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"type:datetime;autoUpdateTime:nano"`
+	// DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:datetime"`
 }
 
 func (m *Base) BeforeCreate(_ *gorm.DB) error {
