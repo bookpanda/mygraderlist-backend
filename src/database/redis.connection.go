1d5a0f5593e53ed10634c7ad92f7a6f7f80e74e1
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-18 17:40:09 +0700

    fix: redis add password

--- src/database/redis.connection.go
+++ src/database/redis.connection.go
@@ -1,15 +1,22 @@
 package database
 
 import (
+	"errors"
+
 	"github.com/bookpanda/mygraderlist-backend/src/config"
 	"github.com/go-redis/redis/v8"
 )
 
 func InitRedisConnect(conf *config.Redis) (cache *redis.Client, err error) {
 	cache = redis.NewClient(&redis.Options{
-		Addr: conf.Host,
-		DB:   3,
+		Addr:     conf.Host,
+		DB:       3,
+		Password: conf.Password,
 	})
+
+	if cache == nil {
+		return nil, errors.New("Cannot connect to redis server")
+	}
 
 	return
 }
